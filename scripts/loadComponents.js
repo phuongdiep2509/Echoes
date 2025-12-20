82b2bd5eb46930676893d0de8ccb0769723bf792
phuongdiep2509 <nguyentranphuongdiep09@gmail.com>
2025-12-21 01:58:26 +0700

    add booking

---
+++ scripts/loadComponents.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#header").load("components/user_header.html");
+    $("#movie-list").load("components/movieList.html");
+    $("#payment").load("components/payment.html");
+});
