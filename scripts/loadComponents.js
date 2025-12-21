b10333005da107a35c50911bbe95196dbf07e392
phamhue220405-ph <phamhue220405@gmail.com>
2025-12-21 22:41:17 +0700

    xóa các trang không cần thiết

--- scripts/loadComponents.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $("#header").load("components/user_header.html");
-    $("#movie-list").load("components/movieList.html");
-    $("#payment").load("components/payment.html");
-});
