dcd142eb68b930fa35cf998982362ae066b18c33
26a4042124-collab <26a4042124@hvnh.edu.vn>
2025-12-21 19:21:35 +0700

    add

--- scripts/liveMusic.js
+++ scripts/liveMusic.js
@@ -36,7 +36,7 @@ function renderTrendingList() {
   console.log('Rendering trending events:', trendingEvents.length);
 
   container.innerHTML = trendingEvents.map(([id, event]) => `
-    <div class="trending-item" onclick="location.href='eventDetail.html?id=${id}&type=live-music'">
+    <div class="trending-item" onclick="location.href='concertDetail.html?id=${id}&type=live-music'">
       <div class="trending-thumb">
         <img src="${event.image}" alt="${event.title}">
       </div>
@@ -67,7 +67,7 @@ function renderEventList() {
   console.log('Rendering events for page', currentPage, ':', pageItems.length);
 
   container.innerHTML = pageItems.map(([id, event]) => `
-    <div class="event-wrapper" onclick="location.href='eventDetail.html?id=${id}&type=live-music'">
+    <div class="event-wrapper" onclick="location.href='concertDetail.html?id=${id}&type=live-music'">
       <div class="event-thumb">
         <img src="${event.image}" alt="${event.title}">
       </div>
